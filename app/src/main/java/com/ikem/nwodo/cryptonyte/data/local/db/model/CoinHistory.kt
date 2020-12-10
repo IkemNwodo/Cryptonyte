ed00a5d09910e092de694fef36cea3de094c672a
Ikem <frank@savics.org>
2020-12-10 01:11:45 +0100

    databinding fixes

--- app/src/main/java/com/ikem/nwodo/cryptonyte/data/local/db/model/CoinHistory.kt
+++
@@ -1,7 +0,0 @@
-package com.ikem.nwodo.cryptonyte.data.local.db.model
-
-
-interface CoinHistory {
-    val history: List<History>
-    val id: Int
-}
--- app/src/main/java/com/ikem/nwodo/cryptonyte/data/local/db/model/CoinsWithHistory.kt
+++
@@ -1,3 +0,0 @@
-package com.ikem.nwodo.cryptonyte.data.local.db.model
-
-data class CoinsWithHistory(val coins: List<Coin>?, val coinHistory24H: List<CoinHistory24H>)
